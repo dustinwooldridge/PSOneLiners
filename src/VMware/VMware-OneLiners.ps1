da29c2e4c3bdddbadb1aa8ad79f7cbeeec4eb2e1
administrator <dwooldridge@republicbank.com>
2017-11-16 16:34:25 -0500

    Signed-off-by: administrator <dwooldridge@republicbank.com>

--- src/VMware/VMware-OneLiners.ps1
+++ src/VMware/VMware-OneLiners.ps1
@@ -4,6 +4,9 @@ Get-View -ViewType VirtualMachine | Where-Object { ($_.Guest.Net | ForEach-Objec
 
 # Find a VM by Mac Address
 get-vm | Get-NetworkAdapter | Where-Object {$_.MacAddress -eq "00:50:56:a8:7f:d5"} | format-list -Property *
+$mac = $null
+$mac = "00:50:56:a8:7f:d5"
+get-vm | Get-NetworkAdapter | Where-Object {$_.MacAddress -eq $mac} | format-list -Property *
 
 # Get list of VM's including VM Name, FQDN and OS
 Get-VM -server $vcenter | Sort | Get-View -Property @("Name", "Guest.HostName", "Config.GuestFullName", "Guest.GuestFullName") | Select -Property Name, @{N = "FQDN"; E = {$_.Guest.HostName}}, @{N = "Configured OS"; E = {$_.Config.GuestFullName}}, @{N = "Running OS"; E = {$_.Guest.GuestFullName}} | Format-Table -AutoSize
