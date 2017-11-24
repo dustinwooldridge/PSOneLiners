c9fbba5eb95572b531363ebd02ccd1ce4099ae6c
administrator <dwooldridge@republicbank.com>
2017-11-24 11:32:26 -0500

    added additional commands

--- src/VMware/VMware-OneLiners.ps1
+++ src/VMware/VMware-OneLiners.ps1
@@ -16,3 +16,9 @@ Get-tag "*" | ForEach-Object {$Tag = $_; get-vm -Tag $_ | ForEach-Object {[pscus
 
 # Get a list of VM's and their ESX Host using wildcards
 Get-VM -Name '*RDP*' | Select Name, VMHost 
+
+
+get-vm | Select Name, Version, VMHost,  | Export-CSV c:\temp\vm-hw-compatibility-report.csv -NoTypeInformation
+
+
+Get-VM -Name '*RDP*' "Name", "Guest.HostName", "Config.GuestFullName", "Guest.GuestFullName") | Select -Property Name, @{N = "FQDN"; E = {$_.Guest.HostName}}, @{N = "Configured OS"; E = {$_.Config.GuestFullName}}, @{N = "Running OS"; E = {$_.Guest.GuestFullName}} | Format-Table -AutoSize
